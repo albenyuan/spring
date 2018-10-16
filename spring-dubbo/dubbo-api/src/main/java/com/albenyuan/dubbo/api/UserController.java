28a3ee971aaf0d57deaecadeed08dd60b488d54c
Alben Yuan <albenyuan@aliyun.com>
2018-10-16 20:30:04 +0800

    consumer

--- spring-dubbo/dubbo-api/src/main/java/com/albenyuan/dubbo/api/UserController.java
+++ spring-dubbo/dubbo-api/src/main/java/com/albenyuan/dubbo/api/UserController.java
@@ -1,11 +1,13 @@
 package com.albenyuan.dubbo.api;
 
+import com.albenyuan.dubbo.data.model.User;
 import com.albenyuan.dubbo.mvc.BaseController;
 import com.albenyuan.dubbo.mvc.BaseResult;
+import com.albenyuan.dubbo.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,14 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController extends BaseController {
 
-
     private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @ResponseBody
+
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/list")
     public BaseResult list() {
         logger.info("success");
         return BaseResult.success();
+    }
+
+    @RequestMapping("/login")
+    public BaseResult<User> login(String username, String password) {
+        User user = userService.login(username, password);
+        return BaseResult.success(user);
     }
 
 
