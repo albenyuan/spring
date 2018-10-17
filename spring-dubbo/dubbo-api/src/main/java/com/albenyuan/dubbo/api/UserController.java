a4bd84755df874f63074f62fe72189ded3e5ab41
Alben Yuan <albenyuan@aliyun.com>
2018-10-17 18:01:33 +0800

    annotation consumer

--- spring-dubbo/dubbo-api/src/main/java/com/albenyuan/dubbo/api/UserController.java
+++ spring-dubbo/dubbo-api/src/main/java/com/albenyuan/dubbo/api/UserController.java
@@ -7,7 +7,6 @@ import com.albenyuan.dubbo.service.UserService;
 import com.alibaba.dubbo.config.annotation.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
