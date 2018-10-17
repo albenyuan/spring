deca2a3125dae89a5625c338e397f9d8ad88312b
Alben Yuan <albenyuan@aliyun.com>
2018-10-17 09:45:03 +0800

    provider

--- spring-dubbo/dubbo-service/src/main/java/com/albenyuan/dubbo/service/impl/UserServiceImpl.java
+++ spring-dubbo/dubbo-service/src/main/java/com/albenyuan/dubbo/service/impl/UserServiceImpl.java
@@ -3,12 +3,13 @@ package com.albenyuan.dubbo.service.impl;
 import com.albenyuan.dubbo.service.UserService;
 import com.albenyuan.dubbo.data.model.User;
 import com.alibaba.dubbo.config.annotation.Service;
+import org.springframework.stereotype.Component;
 
 /**
  * @Author Alben Yuan
  * @Date 2018-10-15 10:50
  */
-@Service
+@Service(interfaceClass = UserService.class)
 public class UserServiceImpl implements UserService {
 
     @Override
