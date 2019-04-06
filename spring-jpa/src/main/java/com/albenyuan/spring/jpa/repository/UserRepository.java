f65d76773260621b39094827a01108e3b33571f5
Alben Yuan <albenyuan@aliyun.com>
2019-04-06 21:20:07 +0800

    temp

---
+++ spring-jpa/src/main/java/com/albenyuan/spring/jpa/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.albenyuan.spring.jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Author Alben Yuan
+ * @Date 2019-03-13 14:23
+ */
+public interface UserRepository extends JpaRepository {
+}
